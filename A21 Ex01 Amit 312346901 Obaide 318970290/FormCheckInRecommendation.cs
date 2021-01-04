bbe4120dd474bc6af652821c2927f958e766191f
Obaida Kata <obaidaka@mta.ac.il>
2021-01-04 08:20:28 +0200

    The issue with formCheckin fixed

--- A21 Ex01 Amit 312346901 Obaide 318970290/FormCheckInRecommendation.cs
+++ A21 Ex01 Amit 312346901 Obaide 318970290/FormCheckInRecommendation.cs
@@ -6,34 +6,19 @@ namespace A21_Ex01_Amit_312346901_Obaide_318970290
 {
     public partial class FormCheckInRecommendation : Form
     {
-        private readonly string r_ImagesTemplateUrl;
-        private readonly string r_VideosTemplateUrl;
-        private readonly string r_WeatherTemplateUrl;
-        private readonly string r_RestaurantsTemplateUrl;
-        private Uri m_URL;
         private string m_Location;
-        private ePage m_CureentPage;
         private AppManager m_AppManager;
-
+        private string[] m_ButtonsNames;
+        private eURLType m_LastURLType;
         public FormCheckInRecommendation()
         {
             InitializeComponent();
             m_AppManager = AppManager.Instance;
             webBrowser.ScriptErrorsSuppressed = true;
-            r_ImagesTemplateUrl = "https://www.google.com/search?q={0}&tbm=isch&q=findSomeImage";
-            r_VideosTemplateUrl = "https://www.google.com/search?q={0}&tbm=vid";
-            r_WeatherTemplateUrl = "https://www.google.com/search?q={0}+weather";
-            r_RestaurantsTemplateUrl = "https://www.yelp.com/search?find_desc=Restaurants&find_loc={0}%2C+CA&ns=1";
             splitContainer1.Dock = DockStyle.Fill;
             fetchCheckIns();
-        }
-
-        private enum ePage
-        {
-            Images,
-            Videos,
-            Weather,
-            Resturants
+            m_ButtonsNames = URLFactory.GetSupportedURLTypes();
+            createButtons();
         }
 
         private void backButton_Click(object sender, EventArgs e)
@@ -42,6 +27,22 @@ namespace A21_Ex01_Amit_312346901_Obaide_318970290
             m_AppManager.MainForm.StartPosition = FormStartPosition.Manual;
             m_AppManager.MainForm.Location = this.Location;
             m_AppManager.MainForm.Show();
+        }
+
+        private void createButtons()
+        {
+            buttonsRow.ColumnCount = m_ButtonsNames.Length;
+            buttonsRow.ColumnStyles.Clear();
+            int width = (100 / buttonsRow.ColumnCount);
+            foreach (string buttonName in m_ButtonsNames)
+            {
+                Button buttonToAdd = new Button();
+                buttonToAdd.Text = buttonName;
+                buttonToAdd.Click += new EventHandler(button_Click);
+                buttonToAdd.Dock = DockStyle.Fill;
+                buttonsRow.Controls.Add(buttonToAdd);
+                buttonsRow.ColumnStyles.Add(new ColumnStyle { SizeType = SizeType.Percent, Width = width });
+            }
         }
 
         private void fetchCheckIns()
@@ -62,7 +63,6 @@ namespace A21_Ex01_Amit_312346901_Obaide_318970290
                 }
                 else
                 {
-                    m_CureentPage = ePage.Images;
                     listBoxCheckIns.SelectedIndex = 0;
                 }
             }
@@ -72,80 +72,34 @@ namespace A21_Ex01_Amit_312346901_Obaide_318970290
             }
         }
 
-        private bool isCheckInEmpty()
-        {
-            return listBoxCheckIns.Items.Count == 0;
-        }
-
-        private void imagesButton_Click(object sender, EventArgs e)
-        {
-            if (!isCheckInEmpty())
-            {
-                m_URL = new Uri(string.Format(r_ImagesTemplateUrl, m_Location));
-                webBrowser.Navigate(m_URL);
-                m_CureentPage = ePage.Images;
-            }
-        }
-
-        private void videosButton_Click(object sender, EventArgs e)
-        {
-            if (!isCheckInEmpty())
-            {
-                m_URL = new Uri(string.Format(r_VideosTemplateUrl, m_Location));
-                webBrowser.Navigate(m_URL);
-                m_CureentPage = ePage.Videos;
-            }
-        }
-
-        private void weatherButton_Click(object sender, EventArgs e)
-        {
-            if (!isCheckInEmpty())
-            {
-                m_URL = new Uri(string.Format(r_WeatherTemplateUrl, m_Location));
-                webBrowser.Navigate(m_URL);
-                m_CureentPage = ePage.Weather;
-            }
-        }
-
-        private void restaurantsButton_Click(object sender, EventArgs e)
-        {
-            if (!isCheckInEmpty())
-            {
-                m_URL = new Uri(string.Format(r_RestaurantsTemplateUrl, m_Location));
-                webBrowser.Navigate(m_URL);
-                m_CureentPage = ePage.Resturants;
-            }
-        }
-
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (sender is ListBox)
             {
                 m_Location = (sender as ListBox).Text.Replace(' ', '+');
-                refreshPage(sender, e);
+                refreshPage();
             }
         }
 
-        private void refreshPage(object i_Sender, EventArgs i_Arguments)
+        private void button_Click(object sender, EventArgs e)
         {
-            switch (m_CureentPage)
+            if (sender is Button)
             {
-                case ePage.Images:
-                    imagesButton_Click(i_Sender, i_Arguments);
-                    break;
-                case ePage.Videos:
-                    videosButton_Click(i_Sender, i_Arguments);
-                    break;
-                case ePage.Weather:
-                    weatherButton_Click(i_Sender, i_Arguments);
-                    break;
-                case ePage.Resturants:
-                    restaurantsButton_Click(i_Sender, i_Arguments);
-                    break;
-                default:
-                    imagesButton_Click(i_Sender, i_Arguments);
-                    break;
+                Button senderAsButton = sender as Button;
+                eURLType type;
+                if (Enum.TryParse<eURLType>(senderAsButton.Text, out type))
+                {
+                    m_LastURLType = type;
+                    refreshPage();
+                }
             }
+        }
+
+        private void refreshPage()
+        {
+            IURL urlX = URLFactory.CreateURL(m_LastURLType);
+            Uri url = urlX.GetURL(m_Location);
+            webBrowser.Navigate(url);
         }
     }
 }
